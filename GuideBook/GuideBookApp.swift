6cf2579b0a061c4feca1f78b14fea330895c4f98
christoseteoglou <itsmeglue@gmail.com>
2023-07-03 13:39:44 +0200

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Christos Eteoglou on 2023-07-03.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
